03840b6ff487e8632186026245b22a4c88bbe233
Resi Respati <resir014@gmail.com>
2017-04-21 13:07:58 +0700

    fix(Video): now properly uses the router props

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -21,7 +21,7 @@ export default {
   name: 'video',
   data () {
     return {
-      id: 'JwMfT2cZGHg',
+      id: this.$route.params.id,
       title: 'What if Barry Bonds had played without a baseball bat? | Chart Party',
       description: 'A must watch even if you don\'t like sports. Jon Bois from SB Nation uses some neat statistics to figure out if baseballer Barry Bonds would still do well, even without carrying a bat.',
       tags: ['sports', 'statistics']
