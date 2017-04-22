a7b2c61742bf99062c9a07fedefaf67fcd5082e2
Resi Respati <resir014@gmail.com>
2017-04-22 14:04:25 +0700

    feat(Videos): proper API handling

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="videos" v-if="videos.length > 0">
-    <paginate class="video-list" name="videos" :list="videos" :per="1">
-      <li v-for="video in paginated('videos')">
-        <app-video class="video-list" v-for="video in paginated('videos')" :key="video-id"
-          v-bind:id="video.id"
-          v-bind:title="video.title"
-          v-bind:description="video.description"
-          v-bind:tags="video.tags"
-          v-bind:contentWarnings="video.contentWarnings"></app-video>
-      </li>
-    </paginate>
-    <paginate-links for="videos" :simple="{ prev: '« previous', next: 'next »' }"></paginate-links>
+  <div class="videos">
+    <div v-if="videos.pending">
+      <p>fetching videos...</p>
+    </div>
+    <div v-if="videos.fulfilled">
+      <paginate class="video-list" name="videos" :list="videos.value" :per="1">
+        <li v-for="video in paginated('videos')">
+          <app-video
+            v-bind:id="video.id"
+            v-bind:title="video.title"
+            v-bind:description="video.description"
+            v-bind:tags="video.tags"
+            v-bind:contentWarnings="video.contentWarnings"></app-video>
+        </li>
+      </paginate>
+      <paginate-links for="videos"
+        :simple="{ prev: '« previous', next: 'next »' }"></paginate-links>
+    </div>
+    <div v-if="videos.rejected">
+      <p>{{ videos.reason.message }}</p>
+    </div>
   </div>
 </template>
 
@@ -19,10 +28,13 @@ import Video from './video.vue'
 
 export default {
   name: 'videos',
-  data: () => ({ videos: {} }),
+  data: () => ({
+    videos: {},
+    paginate: ['videos']
+  }),
   fetch: {
     videos () {
-      return 'https://tilde.town/~resir014/tildetv/api/videos.json'
+      return 'static/api/videos.json'
     }
   },
   components: {
