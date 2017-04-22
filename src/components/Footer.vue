92606242fc73a03884751cfab36fcdbae6cffa15
Resi Respati <resir014@gmail.com>
2017-04-22 12:20:26 +0700

    style(Footer): minor text fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
       designed by <a href="https://tilde.town/~resir014">~resir014</a>. backend work by <a href="https://tilde.town/~karlen">~karlen</a>.
     </small>
     <small>
-      <router-link to="/">back to home</router-link>
+      <a href="https://github.com/resir014/tildetv-frontend" target="_blank">view the code</a>
     </small>
   </footer>
 </template>
