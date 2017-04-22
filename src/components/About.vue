8d6882a94f0e07f046f21db55aa0b54c89ed3bc1
Resi Respati <resir014@gmail.com>
2017-04-22 21:48:46 +0700

    style: fixed wording in about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>about tildetv</h1>
     <p>tildetv is a new way to enjoy media on the internet. each of the videos featured here are curated by <a href="https://tilde.town">tilde.town members</a>, and are available here for everyone's viewing pleasure.</p>
-    <p>videos are reset every week, so you're bound to see something fresh every day!</p>
+    <p>videos are reset every week, so you're bound to see something fresh!</p>
 
     <h2>adding stuff to tildetv</h2>
     <p>coming soon!</p>
