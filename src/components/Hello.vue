1a2be62bc6e904331932aafb6129327b97d78dd8
Resi Respati <resir014@gmail.com>
2017-04-24 15:25:59 +0700

    docs(Hello): updated welcome text

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="hello">
-    <p>tildetv is a new way to enjoy media on the internet. each of the videos featured here are curated by <a href="https://tilde.town">tilde.town members</a>, and are available here for everyone's viewing pleasure.</p>
-    <p>videos are reset every week, so you're bound to see something fresh!</p>
+    <p>tildetv is a new way to enjoy media on the internet. it's a curated playlist of videos added from by members of the <a href="https://tilde.town">tilde.town</a> network. each of the videos featured here are curated by tilde.town members, and are available here for everyone's viewing pleasure.</p>
+
+    <h2>features</h2>
+    <ul>
+      <li>simple-lightweight playlist page</li>
+      <li><router-link to="/howto">add videos to the playlist</router-link> using a simple command line</li>
+      <li>videos are reset every week, so you're bound to see something fresh!</li>
+      <li><a href="https://tilde.town/~karlen/tv/videos.json" target="_blank">JSON datadump</a>, for ~town members who want to develop their own, in-console viewer</li>
+    </ul>
   </div>
 </template>
 
