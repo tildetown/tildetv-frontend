33b86b164279ae5ec0fce1f54b1abc385da814d2
Resi Respati <resir014@gmail.com>
2017-12-20 22:45:51 +0700

    Finalised Video component display

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,30 +7,89 @@ import MediaWrapper from './MediaWrapper';
 import ContentWarningBlock from './ContentWarningBlock';
 
 const VideoItem = styled.article`
-  display: flex;
-  flex-direction: column;
   position: relative;
   margin: 2rem 0;
+
+  @supports (display: grid) {
+    display: grid;
+    grid-template-areas: "video"
+                         "meta";
+    grid-gap: 1rem;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 2fr 1fr;
+      grid-template-areas: "video meta";
+    }
+  }
 `;
 
-const VideoMetadata = styled.section``;
+const VideoWrapper = styled(MediaWrapper)`
+  grid-area: video;
+`;
+
+const VideoMetadata = styled.section`
+  grid-area: meta;
+`;
 
 const VideoTitle = styled.h4`
+  margin-top: 0;
   font-weight: 600;
+`;
+
+const VideoTagWrapper = styled.div`
+  margin: 1rem 0;
+`;
+
+const VideoTagLabel = styled.span`
+  margin-right: .5rem;
+  padding: .125rem .25rem;
+  font-size: 85%;
+  color: inherit;
+  background-color: var(--label-color);
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const VideoTagFooter = styled.footer`
+  margin: 1rem 0;
+
+  p {
+    margin: 0;
+  }
 `;
 
 const Video = ({ video }) => (
   <VideoItem>
-    <MediaWrapper>
+    <VideoWrapper>
       <YouTube
         videoId={video.id.v[0]}
         onReady={e => e.target.pauseVideo()}
       />
-    </MediaWrapper>
+    </VideoWrapper>
     <VideoMetadata>
       {video.title ? <VideoTitle>{video.title}</VideoTitle> : null}
       {video.description ? <p>{video.description}</p> : null}
       {video.contentWarnings.length !== 0 ? <ContentWarningBlock warnings={video.contentWarnings} /> : null}
+      <VideoTagWrapper>
+        {video.tags.map(w => <VideoTagLabel key={w}>{w}</VideoTagLabel>)}
+      </VideoTagWrapper>
+      <VideoTagFooter>
+        <p>
+          Posted by{' '}
+          <a href={`https://tilde.town/~${video.user}`} target="_blank" rel="noopener noreferrer">
+            ~{video.user}
+          </a>{' '}
+          at{' '}
+          {new Date(video.addedtime * 1000).toLocaleDateString()}{' '}
+          {new Date(video.addedtime * 1000).toLocaleTimeString()}
+        </p>
+      </VideoTagFooter>
     </VideoMetadata>
   </VideoItem>
 );
